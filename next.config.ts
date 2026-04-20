592689fc98760d4c9b84a6b86a87696726cf1aa9
jrsem <juniorsemerzier@gmail.com>
2026-04-20 16:17:38 -0400

    presenation page done

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,18 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // This allows all hostnames
+      },
+      {
+        protocol: 'http',
+        hostname: '**', // This allows all hostnames over insecure connections
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
